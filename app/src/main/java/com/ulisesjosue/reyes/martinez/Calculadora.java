503c087fe0d2d1025870975e9bb61bb082dbfb30
reyes-martinez29 <reyes.martinez.ulisesjosue@gmail.com>
2021-11-05 09:44:27 -0600

    version Calculadora

---
+++ app/src/main/java/com/ulisesjosue/reyes/martinez/Calculadora.java
@@ -0,0 +1,19 @@
+package com.ulisesjosue.reyes.martinez;
+
+import java.io.Serializable;
+
+public class Calculadora implements Serializable {
+    public void suma(){
+
+    }
+    public void resta(){
+
+    }
+    public void multiplicacion(){
+
+    }
+    public void divicion(){
+
+    }
+
+}
