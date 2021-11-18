a6fe100f0f4812db90f3bb5fad56f48ba405b88d
reyes-martinez29 <reyes.martinez.ulisesjosue@gmail.com>
2021-11-18 00:52:11 -0600

    Resta

--- app/src/main/java/com/ulisesjosue/reyes/martinez/MainActivity.java
+++ app/src/main/java/com/ulisesjosue/reyes/martinez/MainActivity.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button buttonSuma;
+    private Button buttonResta;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,10 +25,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+        buttonResta = findViewById(R.id.buttonResta );
+        buttonResta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                abrirResta( );
+            }
+        });
     }
 
     private void abrirSuma() {
         Intent intent = new Intent( this , SumaActivity.class);
+        startActivity( intent );
+
+    }
+    private void abrirResta() {
+        Intent intent = new Intent( this , RestaActivity.class );
         startActivity( intent );
     }
 
